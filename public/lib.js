0fa7969bebbf2d43e1be58d5edb120a1cfbd43d3
forechoandlook <zuowangyang@foxmail.com>
2026-05-04 23:59:55 +0800

    fix

--- public/lib.js
+++ public/lib.js
@@ -1,1 +1,2 @@
 export * from 'https://cdn.jsdelivr.net/gh/forechoandlook/mini-react@latest/dist/mini-react.min.js';
+// doc from https://cdn.jsdelivr.net/gh/forechoandlook/mini-react@latest/README.mini.md
